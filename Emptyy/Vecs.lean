cd2565e6169fa7655fded958fbdc7e2aa39cc3a0
Guilherme <guilhermehas@hotmail.com>
2024-01-20 23:09:28 +0100

    started swap involute

--- Emptyy/Vecs.lean
+++ Emptyy/Vecs.lean
@@ -1,17 +1,14 @@
-import Mathlib.Data.Vector
+import Mathlib.Data.Vector.Basic
+import Mathlib.Logic.Function.Basic
 
 variable {m n : ℕ}
 
 namespace Vector
 
-def set : Vector α n → Fin n → α → Vector α n
-  | ⟨xs, _⟩, ⟨i, _⟩, val => ⟨List.set xs i val, by simp [*]⟩
-
 notation:10 xs " [ " i " ]≔ " val  => set xs i val
 
 def swap : Vector α n → Fin n → Fin n → Vector α n
   | xs, i, j => (xs [ i ]≔ get xs j) [ j ]≔ get xs i
-
 
 def setFromList : List (Fin n × α) → Vector α n → Vector α n
   | List.nil, xs => xs
@@ -25,3 +22,17 @@ def listFromIndex (xs : Vector α n) (i j : Fin n) : List (Fin n × α) :=
 
 theorem swapSameSet (xs : Vector α n) (i j : Fin n) :
   setFromList (listFromIndex xs i j) xs = swap xs i j := rfl
+
+def swapAfter : Fin n → Fin n → Vector α n → Vector α n
+  | i, j, xs => swap xs i j
+
+theorem swapAfterSame (xs : Vector α n) (i j : Fin n) :
+  setFromList (listFromIndex xs i j) xs = swapAfter i j xs := rfl
+
+theorem swapInvolute (i j : Fin n) :
+  Function.Involutive (swapAfter i j : Vector α n → Vector α n) := by
+  intros xs
+  apply ext
+  intro k
+  rw [←swapAfterSame xs i j, ←swapAfterSame (setFromList (listFromIndex xs i j) xs) i j]
+  sorry
